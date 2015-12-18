6e722906652d389fcdb30a441074aab6ddcf2667
Jessica Frazelle <acidburn@docker.com>
2015-12-18 11:04:23 -0800

    gofmt
    Signed-off-by: Jessica Frazelle <acidburn@docker.com>

--- forecast/print.go
+++ forecast/print.go
@@ -22,32 +22,32 @@ type UnitMeasures struct {
 var (
 	// UnitFormats describe each regions UnitMeasures.
 	UnitFormats = map[string]UnitMeasures{
-		"us": UnitMeasures{
+		"us": {
 			Degrees:       "°F",
 			Speed:         "mph",
 			Length:        "miles",
 			Precipitation: "in/hr",
 		},
-		"si": UnitMeasures{
+		"si": {
 			Degrees:       "°C",
 			Speed:         "m/s",
 			Length:        "kilometers",
 			Precipitation: "mm/h",
 		},
-		"ca": UnitMeasures{
+		"ca": {
 			Degrees:       "°C",
 			Speed:         "km/h",
 			Length:        "kilometers",
 			Precipitation: "mm/h",
 		},
 		// deprecated, use "uk2" in stead
-		"uk": UnitMeasures{
+		"uk": {
 			Degrees:       "°C",
 			Speed:         "mph",
 			Length:        "kilometers",
 			Precipitation: "mm/h",
 		},
-		"uk2": UnitMeasures{
+		"uk2": {
 			Degrees:       "°C",
 			Speed:         "mph",
 			Length:        "miles",
